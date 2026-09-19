29647009c28b6dc05408f6aa783851dbbea3a049
Antony-005 <antonyochieng056@gmail.com>
2026-09-19 12:56:36 +0300

    Add Google Analytics 4 tracking

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Fraunces, IBM_Plex_Mono } from 'next/font/google';
 import { InquiryProvider } from '@/context/InquiryContext';
+import Script from 'next/script';
 
 const fraunces = Fraunces({ subsets: ['latin'], variable: '--font-fraunces' });
 const plexMono = IBM_Plex_Mono({
@@ -19,6 +20,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${fraunces.variable} ${plexMono.variable}`}>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0C3KD9T85F"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0C3KD9T85F');
+          `}
+        </Script>
         <InquiryProvider>{children}</InquiryProvider>
       </body>
     </html>
